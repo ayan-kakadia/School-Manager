e83411e695ab1a7f48998d3e3751dd6e2b021f38
Ayan Kakadiya <kakadiyaayan123@gmail.com>
2023-10-01 00:36:10 +0530

    added app and config files

---
+++ app.py
@@ -0,0 +1,15 @@
+from controller.main import Controller
+from models.main import Model
+from view.main import View
+
+
+def app():
+    model = Model()
+    view = View()
+    controller = Controller(model, view)
+
+    controller.start()
+
+
+if __name__ == '__main__':
+    app()
---
+++ config.py
@@ -0,0 +1,12 @@
+class Config:
+    BG_COLOR = '#FFFFFF'
+
+    WHITE = '#FFFFFF'
+
+    BLACK = '#000000'
+
+    BLUE = '#4152B3'
+
+    RED = '#CC5348'
+
+    DARK_RED = '#EF3E31'
